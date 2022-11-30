36f2e7471883a034f17e9c6f476948f81ae9ae0e
Youssef Yammine <lebenbou@gmail.com>
2022-11-30 18:45:21 +0200

    moutran nekna passowrd

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,8 +3,8 @@ const { request, response } = require('express')
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
-// const jwt = require("jsonwebtoken")
-// const JWT_SECRET = "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
+const jwt = require("jsonwebtoken")
+const JWT_SECRET = "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
 
 
 const SignUpTemplateCopy = require('../models/singupModels')
@@ -38,16 +38,22 @@ router.post('/signup', async (request, response) => {
 })
 
 router.post("/login", async (req, res) => {
-
-    const email = req.body.email;
+    const { email, password } = req.body;
   
     const user = await SignUpTemplateCopy.findOne({ email });
     if (!user) {
-      return res.json({ message: "User Not found" });
+      return res.json({ error: "User Not found" });
     }
-    else{
-        return res.json({ message:"User Found!" });
+    if (await bcrypt.compare(password, user.password)) {
+      const token = jwt.sign({ email: user.email }, JWT_SECRET);
+  
+      if (res.status(201)) {
+        return res.json({ status: "ok", data: token });
+      } else {
+        return res.json({ error: "error" });
+      }
     }
+    res.json({message: "Invalid pass"})
 });
 
 module.exports = router
